d84eba927ce2449886c6edd298d69bf710ca0efb
adityagaur0 <adityakumargaur0@gmail.com>
2023-09-13 11:23:00 +0530

    changes

---
+++ CB -Lectures codes/Lecture17/2Darray/06DiagonalTravesal.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void printdiagonal(int arr[][10],int m,int n,i,j){
+    for()
+}
+void diagionaltraverse(int arr[][10],int m,int n){
+
+    //start diagonal from rows 0<=rows<=n-1 
+    //1.(i,0)
+    for(int i=0;i<=m-1;i++ ){
+        printdiagonal(arr,m,n,i,0);
+
+    }
+
+    //2.start diagonal from col 0<=col<=n-1 (0,j)
+    for(int j=1;j<=n-1;j++){  //start from one not from zero cuzz to avoid repeatition
+        printdiagonal(arr,m,n,0,j);
+        
+    }
+
+}
+int main(){
+    int arr[10][10]={{11,12,13,14},
+                    {15,16,17,18},
+                    {19,20,21,22}};
+
+    diagionaltraverse(arr,3,4);
+    return 0;
+
+}
