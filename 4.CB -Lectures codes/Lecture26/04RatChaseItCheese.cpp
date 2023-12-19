5258d7b79764bc28a809c007789fd8e2ffd06f0a
adityagaur0 <adityakumargaur0@gmail.com>
2023-12-19 18:39:27 +0000

    20 Dec 2023, 00:09

--- 4.CB -Lectures codes/Lecture26/04RatChaseItCheese.cpp
+++ 4.CB -Lectures codes/Lecture26/04RatChaseItCheese.cpp
@@ -3,54 +3,6 @@
 #include <iostream>
 using namespace std;
 
-// void f(char maze[][10],char path[][10],int m,int n,int i,int j){
-//     //base case
-//     if(i==m||j==n|| i==-1||j==-1){
-//         return;
-//     }
-//     if(i==m-1 && j==n-1){
-//         path[i][j]='1';
-//         for(int i=0;i<=m-1;i++){
-//             for(int j=0;j<=n-1;j++){
-//                 cout<<path[i][j]<<" ";
-//             }
-//             cout<<endl;
-//         }
-//         cout<<endl;
-//         path[i][j]='0';
-//         return;
-//     }
-
-//     if(maze[i][j]=='X'){
-//         return;
-//     }
-
-    // //to avoid infinite recursions
-    // if(path[i][j]=='1'){
-    //     return;
-    // }
-//     //recursive case
-
-//     path[i][j]='1';
-
-//     //move right
-//     f(maze,path,m,n,i,j+1);
-
-//     //move left
-//     f(maze,path,m,n,i,j-1);
-
-//     //move down
-//     f(maze,path,m,n,i+1,j);
-
-//     //move down
-//     f(maze,path,m,n,i-1,j);
-
-//     //backtrack
-//     path[i][j]='0';
-// }
-
-int dy[]={1,-1,0,0}; // on the basis on up down left right i used below
-int dx[]={0,0,1,-1};
 void f(char maze[][10],char path[][10],int m,int n,int i,int j){
     //base case
     if(i==m||j==n|| i==-1||j==-1){
@@ -73,35 +25,83 @@ void f(char maze[][10],char path[][10],int m,int n,int i,int j){
         return;
     }
 
-    // //to avoid infinite recursions
-    // if(path[i][j]=='1'){
-    //     return;
-    // }
+    //to avoid infinite recursions
+    if(path[i][j]=='1'){
+        return;
+    }
     //recursive case
 
-    maze[i][j]='X'; // blocking the maze to avoid infinite recursions
     path[i][j]='1';
 
-    // //move right
-    // f(maze,path,m,n,i,j+1);
+    //move right
+    f(maze,path,m,n,i,j+1);
 
-    // //move left
-    // f(maze,path,m,n,i,j-1);
+    //move left
+    f(maze,path,m,n,i,j-1);
 
-    // //move down
-    // f(maze,path,m,n,i+1,j);
+    //move down
+    f(maze,path,m,n,i+1,j);
 
-    // //move down
-    // f(maze,path,m,n,i-1,j);
-
-    for(int d=0;d<4;d++){
-        f(maze,path,m,n,i+dx[d],j+dy[d]);
-    }
+    //move down
+    f(maze,path,m,n,i-1,j);
 
     //backtrack
     path[i][j]='0';
-    maze[i][j]='0'; // unblocking the maze
 }
+
+// int dy[]={1,-1,0,0}; // on the basis on up down left right i used below
+// int dx[]={0,0,1,-1};
+// void f(char maze[][10],char path[][10],int m,int n,int i,int j){
+//     //base case
+//     if(i==m||j==n|| i==-1||j==-1){
+//         return;
+//     }
+//     if(i==m-1 && j==n-1){
+//         path[i][j]='1';
+//         for(int i=0;i<=m-1;i++){
+//             for(int j=0;j<=n-1;j++){
+//                 cout<<path[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//         cout<<endl;
+//         path[i][j]='0';
+//         return;
+//     }
+
+//     if(maze[i][j]=='X'){
+//         return;
+//     }
+
+//     // //to avoid infinite recursions
+//     // if(path[i][j]=='1'){
+//     //     return;
+//     // }
+//     //recursive case
+
+//     maze[i][j]='X'; // blocking the maze to avoid infinite recursions
+//     path[i][j]='1';
+
+//     // //move right
+//     // f(maze,path,m,n,i,j+1);
+
+//     // //move left
+//     // f(maze,path,m,n,i,j-1);
+
+//     // //move down
+//     // f(maze,path,m,n,i+1,j);
+
+//     // //move down
+//     // f(maze,path,m,n,i-1,j);
+
+//     for(int d=0;d<4;d++){
+//         f(maze,path,m,n,i+dx[d],j+dy[d]);
+//     }
+
+//     //backtrack
+//     path[i][j]='0';
+//     maze[i][j]='0'; // unblocking the maze
+// }
 int main() {
 
 	char maze[][10] = {"0X00",
