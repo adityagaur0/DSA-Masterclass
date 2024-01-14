f0d87ca0848bcf3e579a521b0a4703b81e1dffbd
adityagaur0 <adityakumargaur0@gmail.com>
2024-01-14 22:26:02 +0000

    15 Jan 2024, 03:56

--- 4.CB -Lectures codes/Lecture36_Heap/06_Heap_Sort.cpp
+++ 4.CB -Lectures codes/Lecture36_Heap/06_Heap_Sort.cpp
@@ -7,13 +7,17 @@ using namespace std;
 void heapify(vector<int>& v, int n, int i){
     int max_idx=i;
     int left_child_idx=2*i+1;
-    while(left_child_idx< n and v[left_child_idx]< v[max_idx]){
+    while(left_child_idx< n and v[left_child_idx]> v[max_idx]){
         max_idx =left_child_idx;
 
     }
     int right_child_idx= 2*i+2;
-    while(right_child_idx <n and v[right_child_idx]< v[max_idx]){
+    while(right_child_idx <n and v[right_child_idx]> v[max_idx]){
         max_idx=right_child_idx;
+    }
+    if(max_idx!=i){
+        swap(v[i], v[max_idx]);
+        heapify(v,n,max_idx);
     }
 }
 int main(){
@@ -21,7 +25,7 @@ int main(){
     int n=v.size();
 
     //convert the vector into max heap(bottom uo fashion)
-    for(int i=n-1;i>0;i++){
+    for(int i=n-1;i>=0;i++){
         heapify(v,n,i);
     }
     for(int i: v){
